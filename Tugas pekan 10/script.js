af73d764e0cfdba6385943e36e9858e10e1a1bf9
robbanifawwaz025 <robbanifawwaz025@gmail.com>
2025-12-15 20:27:14 +0700

    Add files via upload

---
+++ Tugas pekan 10/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.question').click(function() {
+        $(this).next('.answer').toggle(300); 
+        $(this).toggleClass('expanded');
+    });
+
+    $('.question').hover(
+        function() {
+            $(this).addClass('question-hover');
+        },
+        function() {
+            $(this).removeClass('question-hover');
+        }
+    );
+
+});
